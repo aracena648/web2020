6a1e1fad3adf8586e2790701dc3d15e27e1f6719
Jose <aracena648@gmail.com>
2020-02-03 10:53:11 -0400

    se actualizo  carpeta de  clases
    se agregaron carpetas face  de  instragram y  de formularios

---
+++ intro/face/action.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+<div class="row well alert alert-success"><?php echo "<pre>";print_R($_POST);?></div>
+</body>
+</html>
